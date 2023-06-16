99dee75a7ded4772faa14cf7ba9566982e7b6d02
John mk UBUNTU <jmakateto@gmail.com>
2023-06-16 13:25:28 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-// Code your solution in this file!
-// returnFirstTwoDrivers
 const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+  return function(value) {
+    return value * multiplier;
   };
-  
-  // returnLastTwoDrivers
-  const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-  };
-  
-  // selectingDrivers
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  // createFareMultiplier
-  const createFareMultiplier = function(integer) {
-    return function(fare) {
-      return fare * integer;
-    };
-  };
-  
-  // fareDoubler
-  const fareDoubler = createFareMultiplier(2);
-  
-  // fareTripler
-  const fareTripler = createFareMultiplier(3);
-  
-  // selectDifferentDrivers
-  const selectDifferentDrivers = function(drivers, selectFunction) {
-    return selectFunction(drivers);
-  };
-  
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, fn) {
+  return fn(drivers);
+};
